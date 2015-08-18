3767191809aa0c509543bfe59c37b27425a71d38
Mike Feijs <mike@feijs.nl>
2015-08-19 01:43:34 +0200

    laravel 4.2 support

--- src/GlobalScope.php
+++ src/GlobalScope.php
@@ -12,21 +12,20 @@ abstract class GlobalScope implements ScopeInterface
      * Apply the scope to a given Eloquent query builder.
      *
      * @param  \Illuminate\Database\Eloquent\Builder $builder
-     * @param  \Illuminate\Database\Eloquent\Model $model
      * @return void
      */
-    abstract public function apply(Builder $builder, Model $model);
+    abstract public function apply(Builder $builder);
 
     /**
      * Remove the scope from the given Eloquent query builder.
      *
      * @param  \Illuminate\Database\Eloquent\Builder $builder
-     * @param  \Illuminate\Database\Eloquent\Model $model
      * @return void
      */
-    public function remove(Builder $builder, Model $model)
+    public function remove(Builder $builder)
     {
         $query = $builder->getQuery();
+        $model = $builder->getModel();
 
         $bindingKey = 0;
 
